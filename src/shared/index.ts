a6b31ed276810d685a2170e4a1e88d3d596e2d92
rahulrajkosta <137993062+rahulrajkosta@users.noreply.github.com>
2024-01-20 12:37:58 +0530

    Update index.ts

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,13 +1,14 @@
 export const config = {
     logoCount: 5,
     firebaseConfig: {
-        apiKey: "AIzaSyDy1o-UQcjwzj2LHKaj5lwiOcU84L6nqg8",
-        authDomain: "rk-singh-video-call.firebaseapp.com",
-        projectId: "rk-singh-video-call",
-        storageBucket: "rk-singh-video-call.appspot.com",
-        messagingSenderId: "1078828061156",
-        appId: "1:1078828061156:web:522c87ff1cd07b5ce837f1",
-        measurementId: "G-JRQ0G2ZHGT"
+          apiKey: "AIzaSyDy1o-UQcjwzj2LHKaj5lwiOcU84L6nqg8",
+          authDomain: "rk-singh-video-call.firebaseapp.com",
+          databaseURL: "https://rk-singh-video-call-default-rtdb.firebaseio.com",
+          projectId: "rk-singh-video-call",
+          storageBucket: "rk-singh-video-call.appspot.com",
+          messagingSenderId: "1078828061156",
+          appId: "1:1078828061156:web:522c87ff1cd07b5ce837f1",
+          measurementId: "G-JRQ0G2ZHGT"
     },
     servers: {
         iceServers: [
