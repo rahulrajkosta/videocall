742510ec8261fab82b448d24113a59c668eaf350
rahulrajkosta <137993062+rahulrajkosta@users.noreply.github.com>
2024-01-20 11:48:04 +0530

    Update index.ts

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,13 +1,13 @@
 export const config = {
     logoCount: 5,
     firebaseConfig: {
-        apiKey: "Your Firbase API Key",
-        authDomain: "Your Firbase authDomain",
-        projectId: "Your Firbase projectId",
-        storageBucket: "Your Firbase storageBucket",
-        messagingSenderId: "Your Firbase messagingSenderId",
-        appId: "Your Firbase appId",
-        measurementId: "Your Firbase measurementId"
+        apiKey: "AIzaSyDy1o-UQcjwzj2LHKaj5lwiOcU84L6nqg8",
+        authDomain: "rk-singh-video-call.firebaseapp.com",
+        projectId: "rk-singh-video-call",
+        storageBucket: "rk-singh-video-call.appspot.com",
+        messagingSenderId: "1078828061156",
+        appId: "1:1078828061156:web:522c87ff1cd07b5ce837f1",
+        measurementId: "G-JRQ0G2ZHGT"
     },
     servers: {
         iceServers: [
@@ -20,4 +20,3 @@ export const config = {
     },
     DEV: "development" === 'development',
 }
-
